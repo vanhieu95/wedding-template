33583f33c5b08c96e02354eacf98766f03f51ddb
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2024-10-20 15:16:40 +0700

    Update section title

--- src/layouts/Section.jsx
+++ src/layouts/Section.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
+import { Favorite } from '@mui/icons-material'
+import { Divider } from '@mui/material'
 
 function Section({ title, children }) {
   return (
-    <div className="mx-auto w-container py-12">
-      <h2 className="text-center font-shantell text-4xl font-bold mb-10">
-        {title}
-      </h2>
+    <div className="mx-auto w-container py-12 text-center">
+      <div className="relative mb-14">
+        <h2 className="font-shantell text-4xl font-bold">{title}</h2>
+        <div className="border-l-2 border-solid border-black h-10 absolute left-1/2 top-12"></div>
+        <Favorite className="absolute left-1/2 -ml-3 top-20" />
+      </div>
 
       {children}
     </div>
