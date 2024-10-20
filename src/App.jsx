78723f3cc0ac8fb064f494dd478ba5ca2df76bdf
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2024-10-20 23:59:40 +0700

    Add section Gift

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Couple from './layouts/Couple'
 import Event from './layouts/Event'
+import Gift from './layouts/Gift'
 import Hero from './layouts/Hero'
 import Section from './layouts/Section'
 
 function App() {
   return (
-    <>
+    <main>
       <Hero />
       <Section title="Cặp đôi" children={<Couple />} />
       <Section title="Sự kiện" children={<Event />} />
-    </>
+      <Section title="Mừng cưới" children={<Gift />} />
+    </main>
   )
 }
 
---
+++ src/layouts/Gift.jsx
@@ -0,0 +1,20 @@
+import GiftItem from '@/components/GiftItem'
+import gifts from '@data/gift'
+import { Stack } from '@mui/material'
+
+function Gift() {
+  return (
+    <Stack
+      direction={'row'}
+      justifyContent={'space-evenly'}
+      useFlexGap
+      sx={{ flexWrap: 'wrap' }}
+    >
+      {gifts.map((gift, i) => (
+        <GiftItem {...gift} key={i} />
+      ))}
+    </Stack>
+  )
+}
+
+export default Gift
