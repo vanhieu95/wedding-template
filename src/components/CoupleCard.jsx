7cc664ca7515141f7ded97ccdcb16f6a83ee5737
Hieu Nguyen <nguyenluongtrunghieu1995@gmail.com>
2024-10-21 00:12:00 +0700

    Update section Couple

--- src/components/CoupleCard.jsx
+++ src/components/CoupleCard.jsx
@@ -13,10 +13,8 @@ function CoupleCard({
   const imageUrl = getImageUrl(image)
 
   return (
-    <Box className={`text-${position}`}>
-      <h3
-        className={`font-shantell text-xl font-bold text-start md:text-${position}`}
-      >
+    <Box sx={{ textAlign: position }}>
+      <h3 className="font-shantell text-xl font-bold text-start mb-4">
         {name}
       </h3>
       <Paper
@@ -34,7 +32,7 @@ function CoupleCard({
             alt={role}
             src={imageUrl}
             sx={{ width: 180, height: 180, borderColor }}
-            className={`border-4 border-double border-gray-700 shadow-md`}
+            className="border-4 border-double border-gray-700 shadow-md"
           />
           <div>{description}</div>
         </Stack>
